f4c415c25568cdc412032e115b3965f4755fa33e
Davi Viana <davianasales@outlook.com>
2025-02-10 12:13:03 -0300

    chore: global defines

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  __NAME__: string
+  __VERSION__: string
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import pkg from './package.json'
 
 import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __NAME__: JSON.stringify(pkg.name),
+    __VERSION__: JSON.stringify(pkg.version),
+  },
   plugins: [vue(), UnoCSS(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
