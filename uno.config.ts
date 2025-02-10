c6875343f7acdaa03f9a5d31259a4eea1a298aa1
Davi Viana <davianasales@outlook.com>
2025-02-10 09:49:09 -0300

    deps(unocss): use wind preset, add fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,19 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetWebFonts, presetWind } from 'unocss'
 
-export default defineConfig({})
+export default defineConfig({
+  presets: [
+    presetWind(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Roboto',
+        mono: ['Fira Code', 'Fira Mono:400,700'],
+      },
+    }),
+  ],
+  extendTheme: [
+    (theme) => {
+      return theme
+    },
+  ],
+})
